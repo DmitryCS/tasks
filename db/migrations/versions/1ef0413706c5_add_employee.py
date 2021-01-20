5da67dfcdd6304f8ec061111d14dac50379d2d86
Dmitry <dmitry.lyzhin2014@yandex.ru>
2021-01-20 13:09:49 +0500

    Аутентификация пользователя

--- db/migrations/versions/1ef0413706c5_add_employee.py
+++ db/migrations/versions/1ef0413706c5_add_employee.py
@@ -1,8 +1,8 @@
-"""Create Employee
+"""add employee
 
-Revision ID: 3728b1613f24
+Revision ID: 1ef0413706c5
 Revises: 
-Create Date: 2020-12-24 17:04:58.000445
+Create Date: 2021-01-20 12:22:48.355532
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '3728b1613f24'
+revision = '1ef0413706c5'
 down_revision = None
 branch_labels = None
 depends_on = None
@@ -22,10 +22,16 @@ def upgrade():
     sa.Column('id', sa.Integer(), autoincrement=True, nullable=False),
     sa.Column('created_at', sa.TIMESTAMP(), nullable=False),
     sa.Column('update_at', sa.TIMESTAMP(), nullable=False),
+    sa.Column('login', sa.VARCHAR(length=20), nullable=False),
+    sa.Column('password', sa.VARBINARY(), nullable=False),
     sa.Column('first_name', sa.VARCHAR(length=50), nullable=True),
     sa.Column('last_name', sa.VARCHAR(length=50), nullable=True),
+    sa.Column('is_delete', sa.BOOLEAN(), nullable=False),
+    sa.Column('position', sa.VARCHAR(length=50), nullable=True),
+    sa.Column('department', sa.VARCHAR(length=50), nullable=True),
     sa.PrimaryKeyConstraint('id'),
-    sa.UniqueConstraint('id')
+    sa.UniqueConstraint('id'),
+    sa.UniqueConstraint('login')
     )
     # ### end Alembic commands ###
 
