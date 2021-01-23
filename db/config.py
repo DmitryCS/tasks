6e1b591a2d429356c54f32495f9da3fa20440db8
Dmitry <dmitry.lyzhin2014@yandex.ru>
2021-01-23 23:47:26 +0500

    23.01.2021

--- db/config.py
+++ db/config.py
@@ -21,6 +21,6 @@ class PostgresConfig:
     name = os.getenv('POSTGRES_NAME', 'tasks')
     user = os.getenv('POSTGRES_USER', 'user1')
     password = os.getenv('POSTGRES_PASSWORD', 'qwerty')
-    host = os.getenv('POSTGRES_HOST', '0.0.0.0')    #tasks-db
+    host = os.getenv('POSTGRES_HOST', 'tasks-db')    #0.0.0.0
     port = os.getenv('POSTGRES_PORT', '5432')
     url = rf'postgresql+psycopg2://{user}:{password}@{host}:{port}/{name}'
